e7da7bb8196b9915d85980342ff0af7cac092d0a
Simone Mauro <simon.mauro@virgilio.it>
2022-01-19 15:10:02 +0100

    Esercizio completato

---
+++ main.js
@@ -0,0 +1,11 @@
+console.log("Js ok");
+
+
+let nome = prompt("Inserisci il tuo nome");
+let cognome = prompt("Inserisci il tuo cognome");
+let colore = prompt("Inserisci il tuo colore preferito");
+
+let numero= "21";
+
+document.getElementById("password").innerHTML = ("La password generata è " + nome + cognome + colore + numero);
+
