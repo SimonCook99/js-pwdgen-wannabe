fe63b73adf36b5aa05bedf27ba33259a1931690c
Simone Mauro <simon.mauro@virgilio.it>
2022-01-19 15:28:22 +0100

    Implementata soluzione alternativa(backtick)

--- main.js
+++ main.js
@@ -1,6 +1,3 @@
-console.log("Js ok");
-
-
 let nome = prompt("Inserisci il tuo nome");
 let cognome = prompt("Inserisci il tuo cognome");
 let colore = prompt("Inserisci il tuo colore preferito");
@@ -9,3 +6,8 @@ let numero= "21";
 
 document.getElementById("password").innerHTML = ("La password generata è " + nome + cognome + colore + numero);
 
+
+
+//soluzione alternativa con il simbolo di backtick
+
+/* document.getElementById("password").innerHTML = (`La password generata è ${nome}${cognome}${colore}${numero}`);*/
